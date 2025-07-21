5ad2910ee5942822444bab1a06194f78cbf29135
KS <56866365+kristensurva@users.noreply.github.com>
2025-07-21 23:26:30 +0300

    Update MushroomLocationRepository.java

--- src/main/java/net/kristen/gis_app/MushroomLocationRepository.java
+++ src/main/java/net/kristen/gis_app/MushroomLocationRepository.java
@@ -3,8 +3,11 @@ package net.kristen.gis_app;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MushroomLocationRepository extends JpaRepository<MushroomLocation, Long> {
+    Optional<MushroomLocation> findByGeoJsonId(String geoJsonId);
     boolean existsByGeoJsonId(String geoJsonId);
     void deleteByGeoJsonId(String geoJsonId);
 }
